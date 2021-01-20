cf00ca86f0dc6cef6730c43fe20917842dd98c51
Alan Middleton <alanmiddleton@uk.ibm.com>
2021-01-20 00:18:58 +0000

    Tuning eslint and prettier with real coe

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier', 'json'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-'use strict';
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
 
-var express       = require('express');
-var fs		  = require('fs');
-var https	  = require('https');
-
-var server;
-var appPort = 8443;
-var contextRoot = "/";
-var app = express();
+let server;
+const appPort = 8443;
+const contextRoot = '/';
+const app = express();
 
 app.get('/**', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 function startupCallback() {
-  console.log('### Running on %s environment ###', process.env.NODE_ENV);
+  const startupMessage =
+    // eslint-disable-next-line no-template-curly-in-string
+    `Express server started on https://localhost:${appPort}${contextRoot} started`;
 
-  var startupMessage = 'Express server started on https://localhost' + ':' + appPort + contextRoot + ' started';
-
+  console.log(`Running on ${process.env.NODE_ENV} environment`);
   console.log(startupMessage);
 }
 
-if (process.env.NODE_ENV === 'LOCAL' ||
+if (
+  process.env.NODE_ENV === 'LOCAL' ||
   process.env.NODE_ENV === undefined ||
-  process.env.NODE_ENV === 'unit-test') {
-  server = https.createServer({
-    key: fs.readFileSync('config/ssl/server.key'),
-    cert: fs.readFileSync('config/ssl/server.cert')
-  }, app).listen(appPort, startupCallback);
+  process.env.NODE_ENV === 'unit-test'
+) {
+  server = https
+    .createServer(
+      {
+        key: fs.readFileSync('config/ssl/server.key'),
+        cert: fs.readFileSync('config/ssl/server.cert'),
+      },
+      app,
+    )
+    .listen(appPort, startupCallback);
 } else {
   server = app.listen(appPort, startupCallback);
 }
 
 module.exports = {
-  app: app,
-  server: server
+  app,
+  server,
 };
